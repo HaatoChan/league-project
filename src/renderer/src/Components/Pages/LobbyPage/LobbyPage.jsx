6b73b51a7c1dd17bafb56849ca45f8e51e50a12d
Pontus Grandin <pg222pb@student.lnu.se>
2023-05-06 02:23:58 +0200

    Splashart styling, image load

--- src/renderer/src/Components/Pages/LobbyPage/LobbyPage.jsx
+++ src/renderer/src/Components/Pages/LobbyPage/LobbyPage.jsx
@@ -36,43 +36,42 @@ const LobbyPage = () => {
 			<div className="teamcomp">
 				<div className="enemytop">			
 					{Array.isArray(teamArray) && teamArray[5]?.championId !== 0 && championIds[teamArray[5]?.championId]?.name}
-					<img src={imgArray[5]?.championImage} alt="" />
+					<img src={imgArray[5]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="enemyjgl">			
 					{Array.isArray(teamArray) && teamArray[6]?.championId !== 0 && championIds[teamArray[6]?.championId]?.name}
-					<img src={imgArray[6]?.championImage} alt="" />
+					<img src={imgArray[6]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="enemymid">				
 					{Array.isArray(teamArray) && teamArray[7]?.championId !== 0 && championIds[teamArray[7]?.championId]?.name}
-					<img src={imgArray[7]?.championImage} alt="" />
+					<img src={imgArray[7]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="enemybot">				
 					{Array.isArray(teamArray) && teamArray[8]?.championId !== 0 && championIds[teamArray[8]?.championId]?.name}
-					<img src={imgArray[8]?.championImage} alt="" />
+					<img src={imgArray[8]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="enemysupp">				
 					{Array.isArray(teamArray) && teamArray[9]?.championId !== 0 && championIds[teamArray[9]?.championId]?.name}
-					<img src={imgArray[9]?.championImage} alt="" />
+					<img src={imgArray[9]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="bluetop">
-					{Array.isArray(teamArray) && teamArray[0]?.championId !== 0 && championIds[teamArray[0]?.championId]?.name}
-					<img src={imgArray[0]?.championImage} alt="" />
+					<img src={imgArray[0]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="bluejgl">			
 					{Array.isArray(teamArray) && teamArray[1]?.championId !== 0 && championIds[teamArray[1]?.championId]?.name}
-					<img src={imgArray[1]?.championImage} alt="" />
+					<img src={imgArray[1]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="bluemid">				
 					{Array.isArray(teamArray) && teamArray[2]?.championId !== 0 && championIds[teamArray[2]?.championId]?.name}
-					<img src={imgArray[2]?.championImage} alt="" />
+					<img src={imgArray[2]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="bluebot">				
 					{Array.isArray(teamArray) && teamArray[3]?.championId !== 0 && championIds[teamArray[3]?.championId]?.name}
-					<img src={imgArray[3]?.championImage} alt="" />
+					<img src={imgArray[3]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 				<div className="bluesupp">				
 					{Array.isArray(teamArray) && teamArray[4]?.championId !== 0 && championIds[teamArray[4]?.championId]?.name}
-					<img src={imgArray[4]?.championImage} alt="" />
+					<img src={imgArray[4]?.championImage} alt="" className='champSplashArt'/>
 				</div>
 			</div>
 			<div className="statistics">
